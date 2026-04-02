8e59826a7b5ee58a4ad45a643205cd9e66a4d556
Vishesh-techno <sonivishesh099@gmail.com>
2026-04-02 16:08:56 +0530

    Two April Code

--- src/TwoApril.java
+++ src/TwoApril.java
@@ -1,6 +1,29 @@
+class Home {
+    int length = 55;
+    int width = 12;
+    int members = 4;
+    int rooms = 2;
+    int kitchen = 1;
+    int washroom = 1;
+
+    void live() {
+        System.out.println(members);
+    }
+
+    void area() {
+        System.out.println(length * width);
+    }
+
+    void totalSpace() {
+        System.out.println(rooms + kitchen + washroom);
+    }
+}
+
 public class TwoApril {
-
     public static void main(String[] args) {
-
+        Home h = new Home();
+        h.live();
+        h.area();
+        h.totalSpace();
     }
 }
