06d1c0c205707bdefc7e75ebd929ccfe76880055
Vishesh-techno <sonivishesh099@gmail.com>
2026-04-03 16:51:50 +0530

    Three April Code

--- src/MyThread.java
+++ src/MyThread.java
@@ -19,18 +19,33 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println("start");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        for (int i = 0; i < 5; i++) {
+            String a = "";
+            for (int j = 0; j < 10000; j++) {
+                a += "a";
+            }
+            System.out.println(Thread.currentThread().getName() + " Priority " + Thread.currentThread().getPriority());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
+    public MyThread(String name) {
+        super(name);
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        MyThread t1 = new MyThread();
-        t1.start();
-        t1.join();
-        System.out.println("I have been waiting for 5 seconds");
+        MyThread l = new MyThread("Low");
+        MyThread m = new MyThread("Medium");
+        MyThread h = new MyThread("HIgh");
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.NORM_PRIORITY);
+        h.setPriority(Thread.MAX_PRIORITY);
+        l.start();
+        m.start();
+        h.start();
     }
 }
