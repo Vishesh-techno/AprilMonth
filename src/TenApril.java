79dec902dd102c24c0933c4dd478f4a27b5ac3e3
Vishesh-techno <sonivishesh099@gmail.com>
2026-04-10 16:51:34 +0530

    TEN APRIL CODE

--- src/TenApril.java
+++ src/TenApril.java
@@ -14,10 +14,30 @@ public class TenApril {
         return sb.toString();
     }
 
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    n--;
+
+                    if (n <= 0) return true;
+                }
+            }
+        }
+        return n <= 0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
         System.out.println(mergeAlternatively(a, b));
+        int[] flowerbed = {1, 0, 0, 0, 1};
+        int n = 1;
+        System.out.println(canPlaceFlowers(flowerbed, n));
     }
 }
