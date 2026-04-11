196b4c2c28d5740b0c95d806a0a61cc90d1eb15c
Vishesh-techno <sonivishesh099@gmail.com>
2026-04-11 14:45:58 +0530

    ELEVEN APRIL CODE

---
+++ src/ElevenApril.java
@@ -0,0 +1,17 @@
+public class ElevenApril {
+    public static String revWords(String s){
+        String[] arr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=arr.length-1; i>=0; i--){
+            if(arr[i].isEmpty()) continue;
+            if(!sb.isEmpty()){
+                sb.append(" ");
+            }
+            sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+
+    }
+}
