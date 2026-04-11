1fe2cf4be690bd634f5f3775decebb0cb89d651d
Vishesh-techno <sonivishesh099@gmail.com>
2026-04-11 14:55:18 +0530

    ELEVEN APRIL CODE

--- src/ElevenApril.java
+++ src/ElevenApril.java
@@ -1,17 +1,19 @@
 public class ElevenApril {
-    public static String revWords(String s){
+    public static String revWords(String s) {
         String[] arr = s.split(" ");
         StringBuilder sb = new StringBuilder();
-        for(int i=arr.length-1; i>=0; i--){
-            if(arr[i].isEmpty()) continue;
-            if(!sb.isEmpty()){
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i].isEmpty()) continue;
+            if (!sb.isEmpty()) {
                 sb.append(" ");
             }
             sb.append(arr[i]);
         }
         return sb.toString();
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        String s = "Vishesh Soni";
+        System.out.println(revWords(s));
     }
 }
