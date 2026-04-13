b7b83d4a24eb538820929e892210687f0226cbf6
Vishesh-techno <sonivishesh099@gmail.com>
2026-04-13 15:16:54 +0530

    THIRTEEN APRIL CODE

--- src/ThirteenApril.java
+++ src/ThirteenApril.java
@@ -15,9 +15,29 @@ public class ThirteenApril {
         }
     }
 
+    public static int containerWithMostWater(int[] nums) {
+        int maxWater = 0;
+        int h = 0, w = 0;
+        int currWater = 0;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            h = Math.min(nums[left], nums[right]);
+            w = right - left;
+            currWater = h * w;
+            maxWater = Math.max(currWater, maxWater);
+            if (nums[left] < nums[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+
     public static void main(String[] args) {
         int[] nums = {0, 1, 0, 3, 12};
         moveZeros(nums);
         System.out.println(Arrays.toString(nums));
+        System.out.println(containerWithMostWater(nums));
     }
 }
